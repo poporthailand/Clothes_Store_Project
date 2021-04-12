549b62c11ef73946c8f8ac9e7d2c01ca6f0b0946
poporthailand <ismat_rr@hotmail.com>
2021-04-12 13:27:01 +0700

    first commit

---
+++ main_page/main.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../stylesheets/main.css">
+</head>
+
+<body>
+    <div class="container">
+
+        <div class="header">
+            <pre style="font-size:0px"> </pre>
+            <h1>Vscreen004</h1>
+
+        </div>
+
+        <div class="menu">
+
+            <button>Main</button>
+            <button>Shop</button>
+            <button>Manage</button>
+            <button>Report</button>
+            <button>Contact</button>
+        </div>
+
+        <div class="Body">
+
+            <button class="btn" style="background: #873395;"><h2>s</h2><h3>hop</h3><pre>         </pre><h4>&</h4></button>
+            <button class="btn" style="background: #fe1c1d;">s</button>
+            <button class="btn" style="background: #fdb046;">s</button>
+
+
+
+        </div>
+
+    </div>
+</body>
+
+</html>
