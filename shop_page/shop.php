aeb7ef0cd2736aa5a16dbdc3e92047d1bf369dbf
poporthailand <ismat_rr@hotmail.com>
2021-04-12 15:20:01 +0700

    save

--- shop_page/shop.php
+++ shop_page/shop.php
@@ -13,12 +13,12 @@
     <div class="container">
 
         <div class="header">
-        <pre style="font-size:0px"> </pre>
+            <pre style="font-size:0px"> </pre>
             <h1>Shop</h1>
         </div>
 
         <div class="menu">
-        <a href="../main_page/main.php"><button>Main</button></a>
+            <a href="../main_page/main.php"><button>Main</button></a>
             <a href="#"><button>Shop</button></a>
             <a href="../manage_page/manage.php"><button>Manage</button></a>
             <button>Report</button>
@@ -27,14 +27,32 @@
 
         <div class="Body">
             <div class="Bodyleft">
-                <button class="btn" style="margin-top: 120px;background-image: url('../Image/shop/shirt.png');"></button>
+                <button class="btn"
+                    style="margin-top: 120px;background-image: url('../Image/shop/shirt.png');"></button>
                 <button class="btn" style=" background-image: url('../Image/shop/pant.png');"></button>
                 <button class="btn" style=" background-image: url('../Image/shop/etc.png');"></button>
                 <button class="btn" style=" background-image: url('../Image/shop/record.png');"></button>
             </div>
 
             <div class="Bodyright">
-                
+                <div class="number">
+
+                    <div class="record" style="margin-left: 70px;"></div>
+
+                </div>
+
+                <div class="number" style="background-color:white" >
+                <div class="record"></div>
+                </div>
+
+                <div class="number">
+                <div class="record"></div>
+                </div>
+                <div class="number" style="background-color:white" >
+                <div class="record"></div>
+                </div>
+                <br>
+
             </div>
         </div>
 
