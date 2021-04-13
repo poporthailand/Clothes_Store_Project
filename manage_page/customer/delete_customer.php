062c40f52a0ab35a2d861f975b45860a55129098
poporthailand <ismat_rr@hotmail.com>
2021-04-13 13:22:02 +0700

    save

---
+++ manage_page/customer/delete_customer.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../stylesheets/manage.css">
+</head>
+
+<body>
+    <div class="container">
+
+        <div class="header">
+        <pre style="font-size:0px"> </pre>
+            <h1>Vscreen004</h1>
+        </div>
+
+        <div class="menu">
+        <a href="../main_page/main.php"><button>Main</button></a>
+            <a href="../shop_page/shop_shirt.php"><button>Shop</button></a>
+            <a href="#"><button>Manage</button></a>
+            <button>Report</button>
+            <button>Contact</button>
+        </div>
+
+        <div class="Body">
+
+        
+
+            
+        </div>
+
+    </div>
+</body>
+
+</html>
---
+++ manage_page/customer/insert_customer.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../stylesheets/manage.css">
+</head>
+
+<body>
+    <div class="container">
+
+        <div class="header">
+        <pre style="font-size:0px"> </pre>
+            <h1>Vscreen004</h1>
+        </div>
+
+        <div class="menu">
+        <a href="../main_page/main.php"><button>Main</button></a>
+            <a href="../shop_page/shop_shirt.php"><button>Shop</button></a>
+            <a href="#"><button>Manage</button></a>
+            <button>Report</button>
+            <button>Contact</button>
+        </div>
+
+        <div class="Body">
+        <pre style="font-size:0px;"> </pre>
+        <p align=center style="font-size:50px;font-weight:bold;">Make Order</p>
+            
+            <table align=center style="font-size:25px;font-weight:bold;">
+                
+                <tr>
+                    <td>รหัสสินค้า :</td>
+                    <td>
+                    <input tpye=text style="width:120px;">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>รหัสพนักงาน :</td>
+                    <td>
+                    <input tpye=text style="width:120px;">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>รหัสลูกค้า :</td>
+                    <td>
+                    <input tpye=text style="width:120px;">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>รหัสสินค้า :</td>
+                    <td>
+                    <input tpye=text style="width:120px;">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>จำนวนสินค้า :</td>
+                    <td>
+                    <input tpye=text style="width:120px;">
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>ราคาต่อหน่วย :</td>
+                    <td>
+                    <input tpye=text style="width:120px;">
+                    </td>
+                </tr>
+
+            </table>
+            <br>
+            <center><input type="submit" value=Make style="cursor: pointer;font-weight: bold;box-shadow:0 2px rgba(0, 0, 0, 0.2);border: 1px solid #111;width:150px;height:50px;background:#a9d8ea;"></center>
+
+            
+        </div>
+
+    </div>
+</body>
+
+</html>
